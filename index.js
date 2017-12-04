a015cb421e0940d11c50af698b32dd5db780be6d
David Hinote <drhinote@gmail.com>
2017-12-04 17:21:00 -0600

    Adding code files

---
+++ index.js
@@ -0,0 +1,51 @@
+var PeerGroup = require('bitcoin-net').PeerGroup;
+var Inventory = require('bitcoin-inventory');
+var express = require('express');
+var bodyParser = require('body-parser');
+var messages = require('./messages');
+
+var app = express();
+var port = process.env.PORT || 80;
+
+app.use(bodyParser.json({ strict: false }));
+
+var peers = new PeerGroup({
+    dnsSeeds: ["dnsseed01.bongger.com", "dnsseed02.bongger.com", "dnsseed03.bongger.com", "dnsseed04.bongger.com"],
+    magic: 0xc0c0c0c0,
+    defaultPort: 7071,
+    messages: messages.defaultMessages,
+}, {
+        numPeers: 8,
+        acceptIncoming: true,
+        peerOpts: {
+            requireBloom: true,
+            userAgent: "/theGreensCard:1.0.1.0/",
+            relay: false
+        }
+    });
+
+peers.connect();
+
+peers.on('peer', peer => {
+    console.log('Connected to peer');
+});
+
+var inv = Inventory(peers);
+
+function broadcast(tx) {
+    inv.broadcast(tx);
+}
+
+app.route('/tx').post(async (req, res) => {
+    try {
+        console.log(new Date().toLocaleString() + ', Relaying tx: ' + req.body.tx);
+        broadcast(bitcoin.Transaction.fromHex(req.body.tx));
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+app.listen(port);
+
+console.log(new Date().toLocaleTimeString() + ' Server started on: ' + port);
